7ac7f014bbff7cc63788566dac43449e498a0356
Lastwyn <davesoregi@gmail.com>
2022-11-14 11:43:23 +0100

    fel

--- Gyümölcs/Form1.Designer.cs
+++ Gyümölcs/Form1.Designer.cs
@@ -116,9 +116,9 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(12, 197);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.Size = new System.Drawing.Size(101, 13);
             this.label4.TabIndex = 14;
-            this.label4.Text = "Mennyiség:";
+            this.label4.Text = "Mennyiadasdasség:";
             // 
             // label3
             // 
